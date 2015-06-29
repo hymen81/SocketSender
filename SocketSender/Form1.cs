ca790a4826a451e19ec968b3b7d6412a6fa8800c
hymen81 <hymen81@hotmail.com>
2015-06-29 11:55:06 +0800

    1

---
+++ SocketSender/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebCam_Capture;
+
+namespace SocketSender
+{
+    //Design by Pongsakorn Poosankam
+    class WebCam
+    {
+        private WebCamCapture webcam;
+        private System.Windows.Forms.PictureBox _FrameImage;
+        private int FrameNumber = 30;
+        public void InitializeWebCam(ref System.Windows.Forms.PictureBox ImageControl)
+        {
+            webcam = new WebCamCapture();
+            webcam.FrameNumber = ((ulong)(0ul));
+            webcam.TimeToCapture_milliseconds = FrameNumber;
+            webcam.ImageCaptured += new WebCamCapture.WebCamEventHandler(webcam_ImageCaptured);
+            _FrameImage = ImageControl;
+        }
+
+        void webcam_ImageCaptured(object source, WebcamEventArgs e)
+        {
+            _FrameImage.Image = e.WebCamImage;
+        }
+
+        public void Start()
+        {
+            webcam.TimeToCapture_milliseconds = FrameNumber;
+            webcam.Start(0);
+        }
+
+        public void Stop()
+        {
+            webcam.Stop();
+        }
+
+        public void Continue()
+        {
+            // change the capture time frame
+            webcam.TimeToCapture_milliseconds = FrameNumber;
+
+            // resume the video capture from the stop
+            webcam.Start(this.webcam.FrameNumber);
+        }
+
+        public void ResolutionSetting()
+        {
+            webcam.Config();
+        }
+
+        public void AdvanceSetting()
+        {
+            webcam.Config2();
+        }
+
+    }
+
+
+    public partial class Form1 : Form
+    {
+        SocketClient sc;
+        public Form1()
+        {
+            InitializeComponent();
+             sc = new SocketClient(this,"192.168.1.107", 5000);
+             webcam = new WebCam();
+             webcam.InitializeWebCam(ref pictureBox2);
+        }
+        WebCam webcam;
+        public void addListBoxString(string s) 
+        {
+            listBox1.Items.Add(s);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            pictureBox1.Image = pictureBox2.Image;
+           // sc.Connect();
+          //
+           // sc.SendByByte(rgbValues);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sc.Connect();
+            //sc.Send(textBox1.Text);
+            sc.SendByByte(rgbValues);
+           // sc.Send(imageString);
+        }
+
+        string imageString = "";
+        static byte[] rgbValues;
+        unsafe void bmpToChar(Image image)
+        {
+            Bitmap labeledBitmap = null;
+            try
+            {
+                labeledBitmap = new Bitmap(image);
+            }
+            catch (Exception)
+            {
+                image.Dispose();
+                return;
+            }
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+            BitmapData bitmapdata = labeledBitmap.LockBits(rect, ImageLockMode.ReadWrite, labeledBitmap.PixelFormat);
+            IntPtr ptr = bitmapdata.Scan0;
+            int bytes = Math.Abs(bitmapdata.Stride) * image.Height;
+            rgbValues = new byte[bytes];
+            Marshal.Copy(ptr, rgbValues, 0, bytes);
+          /*  for (int i = 0; i < rgbValues.Length; i++)
+            {
+                imageString += rgbValues[i].ToString();
+                listBox1.Items.Add(rgbValues[i].ToString());
+            }*/
+            imageString =System.Text.Encoding.Default.GetString(rgbValues);
+
+            StringBuilder Sb = new StringBuilder();
+            for (int i = 0; i < rgbValues.Length; i++)
+            {
+                if (rgbValues[i] == 0)
+                    rgbValues[i] = 1;
+            }
+          //  Bitmap oBitmap = BitmapFactory
+           // pictureBox2.Image = BufferToImage(rgbValues);
+
+            listBox1.Items.Add(Sb);
+        }
+
+        /// <summary>
+        /// 將 Image 轉換為 Byte 陣列。
+        /// </summary>
+        /// <param name="Image">Image 。</param>
+        /// <param name="imageFormat">指定影像格式。</param>        
+        public  byte[] ImageToBuffer(Image Image, System.Drawing.Imaging.ImageFormat imageFormat)
+        {
+            if (Image == null) { return null; }
+            byte[] data = null;
+            using (MemoryStream oMemoryStream = new MemoryStream())
+            {
+                //建立副本
+                using (Bitmap oBitmap = new Bitmap(Image))
+                {
+                    //儲存圖片到 MemoryStream 物件，並且指定儲存影像之格式
+                    oBitmap.Save(oMemoryStream, imageFormat);
+                    //設定資料流位置
+                    oMemoryStream.Position = 0;
+                    //設定 buffer 長度
+                    data = new byte[oMemoryStream.Length];
+                    
+                    //將資料寫入 buffer
+                    oMemoryStream.Read(data, 0, Convert.ToInt32(oMemoryStream.Length));
+                    //將所有緩衝區的資料寫入資料流
+                    oMemoryStream.Flush();
+                }
+            }
+            return data;
+        }
+
+        /// <summary>
+        /// 將 Byte 陣列轉換為 Image。
+        /// </summary>
+        /// <param name="Buffer">Byte 陣列。</param>        
+        public static Image BufferToImage(byte[] Buffer)
+        {
+            if (Buffer == null || Buffer.Length == 0) { return null; }
+            byte[] data = null;
+            Image oImage = null;
+            Bitmap oBitmap = null;
+            //建立副本
+            data = (byte[])Buffer.Clone();
+            try
+            {
+                MemoryStream oMemoryStream = new MemoryStream(Buffer);
+                //設定資料流位置
+                oMemoryStream.Position = 0;
+                oImage = System.Drawing.Image.FromStream(oMemoryStream);
+                //建立副本
+                oBitmap = new Bitmap(oImage);
+            }
+            catch
+            {
+                throw;
+            }
+            //return oImage;
+            return oBitmap;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string fileName = openFileDialog1.FileName;
+            pictureBox1.Image = Image.FromFile(fileName);
+           // byte[] ss = ImageToBuffer(pictureBox1.Image, System.Drawing.Imaging.ImageFormat.Png);
+
+            
+            /*
+            string byteStr = "";
+            StringBuilder Sb = new StringBuilder();
+            for (int i = 0; i < ss.Length; i++)
+            {
+                Sb.Append(ss[i].ToString());
+            }
+
+            listBox1.Items.Add(Sb);*/
+
+
+            bmpToChar(pictureBox1.Image);
+           
+           // pictureBox2.Image = BufferToImage(ss);
+             
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webcam.Start();
+         //   webcam.Continue();
+        }
+
+        
+    }
+}
---
+++ SocketSender/SocketClinet.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketSender
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using System.Net;
+    using System.Net.Sockets;
+
+
+
+    class SocketClient
+    {
+        #region private property
+
+        /// 
+        /// 遠端 socket server IP 位址
+        /// 
+        private string _RemoteIpAddress;
+
+        /// 
+        /// 遠端 socket server 所監聽的 port number
+        /// 
+        private int _RemotePortNumber;
+
+
+        /// 
+        /// socket client 物件(連接遠端 socket server 用)
+        /// 
+        private TcpClient _TcpClient;
+
+        #endregion
+
+
+        #region public static property
+
+        public static List<string> sMessages = new List<string>();
+        private delegate void SetListBox(string s);
+
+        SetListBox setListBox;
+
+
+        #endregion
+
+
+        #region constructor
+
+        Form1 f;
+
+        public SocketClient(Form1 form,string inRemoteIpAddr, int inRemotePortNum)
+        {
+            this._RemoteIpAddress = inRemoteIpAddr;
+            this._RemotePortNumber = inRemotePortNum;
+            f = form;
+            setListBox = f.addListBoxString;
+        }
+
+        #endregion
+
+
+        #region public method
+
+        /// 
+        /// 連線至 socket server
+        /// 
+        public void Connect()
+        {
+            //初始化 socket client
+            _TcpClient = new TcpClient();
+            _TcpClient.Connect(_RemoteIpAddress, _RemotePortNumber);
+            f.Invoke(setListBox, new object[] { "Client Socket Program - Server Connected ..." });
+          //  sMessages.Add("Client Socket Program - Server Connected ...");
+        }
+
+
+        public string SendByByte(byte[] outStream) 
+        {
+            try
+            {
+                //取得用來傳送訊息至 socket server 的 stream 物件
+                NetworkStream serverStream = _TcpClient.GetStream();
+
+     
+
+                //將資料寫入 stream object (表示傳送資料至 socket server)
+                serverStream.Write(outStream, 0, outStream.Length);
+              //  serverStream.Flush();
+
+                //讀取 socket server 回傳值並轉為 string
+                byte[] inStream = new byte[10025];
+                serverStream.Read(inStream, 0, (int)_TcpClient.ReceiveBufferSize);
+                string returndata = System.Text.Encoding.UTF8.GetString(inStream);
+                f.Invoke(setListBox, new object[] { "Data from Server : " + returndata });
+                //sMessages.Add("Data from Server : " + returndata);
+                return returndata;
+            }
+            catch (Exception exp)
+            {
+                sMessages.Add(exp.ToString());
+                return exp.ToString();
+            }
+        
+        }
+
+
+        public string Send(string inMessage)
+        {
+            try
+            {
+                //取得用來傳送訊息至 socket server 的 stream 物件
+                NetworkStream serverStream = _TcpClient.GetStream();
+
+                //將資料轉為 byte[]
+                byte[] outStream = System.Text.Encoding.UTF8.GetBytes(inMessage);
+
+                //將資料寫入 stream object (表示傳送資料至 socket server)
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+
+                //讀取 socket server 回傳值並轉為 string
+                byte[] inStream = new byte[10025];
+                serverStream.Read(inStream, 0, (int)_TcpClient.ReceiveBufferSize);
+                string returndata = System.Text.Encoding.UTF8.GetString(inStream);
+                f.Invoke(setListBox, new object[] { "Data from Server : " + returndata });
+                //sMessages.Add("Data from Server : " + returndata);
+                return returndata;
+            }
+            catch (Exception exp)
+            {
+                sMessages.Add(exp.ToString());
+                return exp.ToString();
+            }
+        }
+
+        #endregion
+    }
+}
+
